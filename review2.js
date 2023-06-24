ad51100ec14337449a4f3af0987b2130e8027b4f
Vrushali Joshi <vrushalijoshi589@gmail.com>
2023-06-24 10:47:54 +0530

    Review 2 code

---
+++ review2.js
@@ -0,0 +1,22 @@
+function AddTwoNumber(number1, number2) {
+
+    return new Promise((resolve, reject) => {
+        let sum = number1 + number2
+        if (sum < 0) {
+            reject("error:sum is negative")
+        }
+        else {
+            resolve(sum);
+        }
+    });
+
+}
+AddTwoNumber(-5, -10)
+    .then(function (result) {
+        console.log(result);
+
+    })
+    .catch(function (error) {
+        console.log(error)
+    }
+    )
